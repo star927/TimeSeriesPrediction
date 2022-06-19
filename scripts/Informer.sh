e54c89991e0cb0b0f54e4e85625daec365783f59
star <1469025384@qq.com>
2022-06-19 15:54:39 +0800

    Modify Informer.sh

--- scripts/Informer.sh
+++ scripts/Informer.sh
@@ -5,6 +5,9 @@ python -u Informer.py --model informer --data ETT --features M --seq_len 96 --la
 python -u Informer.py --model informer --data ETT --features M --seq_len 168 --label_len 168 --pred_len 168 --e_layers 2 --d_layers 1 --attn prob --distil --exp_num 5
 
 ### ETT S
+python -u Informer.py --model informer --data ETT --features S --seq_len 96 --label_len 48 --pred_len 24 --e_layers 2 --d_layers 1 --attn prob --distil --exp_num 5
+python -u Informer.py --model informer --data ETT --features S --seq_len 96 --label_len 48 --pred_len 48 --e_layers 2 --d_layers 1 --attn prob --distil --exp_num 5
+
 python -u Informer.py --model informer --data ETT --features S --seq_len 720 --label_len 168 --pred_len 24 --e_layers 2 --d_layers 1 --attn prob --distil --exp_num 5
 python -u Informer.py --model informer --data ETT --features S --seq_len 720 --label_len 168 --pred_len 48 --e_layers 2 --d_layers 1 --attn prob --distil --exp_num 5
 python -u Informer.py --model informer --data ETT --features S --seq_len 720 --label_len 168 --pred_len 96 --e_layers 2 --d_layers 1 --attn prob --distil --exp_num 5
@@ -70,3 +73,21 @@ python -u Informer.py --model informerstack --data Weather_WH --features MS --se
 python -u Informer.py --model informerstack --data Weather_WH --features MS --seq_len 64 --label_len 32 --pred_len 16 --s_layers "[3, 2, 1]" --inp_lens "[0, 1, 2]" --d_layers 1 --attn prob --distil --exp_num 3
 python -u Informer.py --model informerstack --data Weather_WH --features MS --seq_len 32 --label_len 32 --pred_len 16 --s_layers "[3, 2, 1]" --inp_lens "[0, 1, 2]" --d_layers 1 --attn prob --distil --exp_num 3
 ### 参数敏感性实验
+
+
+### 时间效率
+python -u Informer.py --model informer --data Weather_WH --features MS --seq_len 512 --label_len 32 --pred_len 16 --e_layers 2 --d_layers 1 --attn prob --distil --exp_num 3 --train_epochs 1
+python -u Informer.py --model informer --data Weather_WH --features MS --seq_len 512 --label_len 32 --pred_len 16 --e_layers 2 --d_layers 1 --attn full --distil --exp_num 3 --train_epochs 1
+python -u Informer.py --model informer --data Weather_WH --features MS --seq_len 256 --label_len 32 --pred_len 16 --e_layers 2 --d_layers 1 --attn prob --distil --exp_num 3 --train_epochs 1
+python -u Informer.py --model informer --data Weather_WH --features MS --seq_len 256 --label_len 32 --pred_len 16 --e_layers 2 --d_layers 1 --attn full --distil --exp_num 3 --train_epochs 1
+
+python -u Informer.py --model informer --data Weather_WH --features MS --seq_len 128 --label_len 32 --pred_len 16 --e_layers 2 --d_layers 1 --attn prob --distil --exp_num 3 --train_epochs 1
+python -u Informer.py --model informer --data Weather_WH --features MS --seq_len 128 --label_len 32 --pred_len 16 --e_layers 3 --d_layers 1 --attn prob --distil --exp_num 3 --train_epochs 1
+python -u Informer.py --model informer --data Weather_WH --features MS --seq_len 128 --label_len 32 --pred_len 16 --e_layers 4 --d_layers 1 --attn prob --distil --exp_num 3 --train_epochs 1
+python -u Informer.py --model informer --data Weather_WH --features MS --seq_len 128 --label_len 32 --pred_len 16 --e_layers 5 --d_layers 1 --attn prob --distil --exp_num 3 --train_epochs 1
+
+python -u Informer.py --model informer --data Weather_WH --features MS --seq_len 128 --label_len 32 --pred_len 16 --e_layers 2 --d_layers 1 --attn prob --exp_num 3 --train_epochs 1
+python -u Informer.py --model informer --data Weather_WH --features MS --seq_len 128 --label_len 32 --pred_len 16 --e_layers 3 --d_layers 1 --attn prob --exp_num 3 --train_epochs 1
+python -u Informer.py --model informer --data Weather_WH --features MS --seq_len 128 --label_len 32 --pred_len 16 --e_layers 4 --d_layers 1 --attn prob --exp_num 3 --train_epochs 1
+python -u Informer.py --model informer --data Weather_WH --features MS --seq_len 128 --label_len 32 --pred_len 16 --e_layers 5 --d_layers 1 --attn prob --exp_num 3 --train_epochs 1
+### 时间效率
